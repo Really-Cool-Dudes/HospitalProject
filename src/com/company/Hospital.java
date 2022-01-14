3f594f3a024b72f565db3ca22b452d5d8205df6f
Ghassen Jemaî <hiallnigas@gmail.com>
2022-01-14 18:57:12 +0100

    blueprint

---
+++ src/com/company/Hospital.java
@@ -0,0 +1,5 @@
+package com.company;
+
+public class Hospital {
+    private String nom;
+}
---
+++ src/com/company/Medecin.java
@@ -0,0 +1,7 @@
+package com.company;
+
+public class Medecin extends Person{
+    private String nom;
+    private String diplome;
+    private Boolean auth;
+}
