d93e33beefc42060617e65504ee0ea6e35eecec3
Ghassen Jemaî <hiallnigas@gmail.com>
2022-01-14 19:33:09 +0100

    fixes

--- src/com/company/Medecin.java
+++ src/com/company/Medecin.java
@@ -1,7 +1,11 @@
 package com.company;
 
-public class Medecin extends Person{
+public class Medecin extends Employee{
     private String nom;
     private String diplome;
     private Boolean auth;
+
+    void masser(Patient patient){
+
+    }
 }
